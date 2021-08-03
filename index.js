2599e8b98a4699bad9184128b10edf376dcd8291
FletchyAF <joshua.f1998@gmail.com>
2021-08-03 13:23:40 -0400

    commit tree a4edd6a45359d818a46ba9e803344321147e1dda

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let writeCards = function(name, event) {
+    let arrayOfThanks = []
+    for (let i = 0; i < name.length; i++) {
+    arrayOfThanks.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+}
+return arrayOfThanks;
+}
+
+let countDown = function(numberCount) {
+    while(numberCount > 0) {
+        console.log(numberCount);
+        numberCount -= 1;
+    }
+    console.log(numberCount)
+}
